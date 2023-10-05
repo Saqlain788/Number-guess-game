2a460e8fbae1506cd17659e80c72c2a32a3a9e79
Saqlain788 <saqlain788@gmail.com>
2023-10-05 15:58:09 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+console.log(chalk.blue("Game: Lets guess a number between 1 to 10 in 3 tries"));
+let randomNumber: number = 7;
+interface AnswersType {
+  myGuessNumber: number;
+}
+const answers: AnswersType = await inquirer.prompt([
+  {
+    type: "number",
+    name: "myGuessNumber",
+    message: "Enter a number between 1 to 10",
+  },
+]);
+
+console.log(answers);
+
+if (answers.myGuessNumber === randomNumber) {
+  console.log(chalk.yellow("You guessed it right"));
+} else {
+  console.log(chalk.red("You guessed it wrong"));
+}
