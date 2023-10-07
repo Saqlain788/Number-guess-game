5de447d0a04e5b1b2d1f540632217cbaaf84cb6a
Saqlain788 <139763515+Saqlain788@users.noreply.github.com>
2023-10-07 04:06:20 -0700

    updates

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
 console.log(chalk.blue("Game: Lets guess a number between 1 to 10 in 3 tries"));
-let randomNumber = 7;
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+let UserGuess = randomNumber;
+console.log(UserGuess);
 let numTries = 3;
-while (numTries > 0) {
-    const answers = await inquirer.prompt([
+let play = true;
+while (play) {
+    while (numTries > 0) {
+        const answers = await inquirer.prompt([
+            {
+                type: "number",
+                name: "myGuessNumber",
+                message: "Enter a number between 1 to 10",
+            },
+        ]);
+        console.log(answers);
+        if (answers.myGuessNumber === UserGuess) {
+            console.log(chalk.yellow("Hurray! You guessed it right, Game Ends"));
+            numTries = 0;
+        }
+        else {
+            console.log(chalk.red("You guessed it wrong"));
+            if (UserGuess > answers.myGuessNumber) {
+                console.log("Think Higher ");
+            }
+            else {
+                console.log("Think Lower ");
+            }
+            console.log(`you have ${numTries - 1} left`);
+        }
+        numTries--;
+    }
+    const playAgainAnswers = await inquirer.prompt([
         {
-            type: "number",
-            name: "myGuessNumber",
-            message: "Enter a number between 1 to 10",
+            type: "confirm",
+            name: "playagain",
+            message: "Do you want to play again",
         },
     ]);
-    console.log(answers);
-    if (answers.myGuessNumber === randomNumber) {
-        console.log(chalk.yellow("You guessed it right"));
+    console.log("Play Again", playAgainAnswers);
+    if (playAgainAnswers.playagain === true) {
+        randomNumber = Math.floor(Math.random() * 10 + 1);
+        UserGuess = randomNumber;
+        numTries = 3;
     }
     else {
-        console.log(chalk.red("You guessed it wrong"));
+        play = false;
+        console.log("Existing Game");
     }
-    console.log(`you have ${numTries} left`);
-    numTries--;
 }
--- index.ts
+++ index.ts
@@ -2,28 +2,61 @@ import chalk from "chalk";
 import inquirer from "inquirer";
 
 console.log(chalk.blue("Game: Lets guess a number between 1 to 10 in 3 tries"));
-let randomNumber: number = 7;
+
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+let UserGuess: number = randomNumber;
+console.log(UserGuess);
+
 interface AnswersType {
   myGuessNumber: number;
 }
+interface playAgainType {
+  playagain: boolean;
+}
 let numTries: number = 3;
-while (numTries > 0) {
-  const answers: AnswersType = await inquirer.prompt([
+let play: boolean = true;
+while (play) {
+  while (numTries > 0) {
+    const answers: AnswersType = await inquirer.prompt([
+      {
+        type: "number",
+        name: "myGuessNumber",
+        message: "Enter a number between 1 to 10",
+      },
+    ]);
+
+    console.log(answers);
+
+    if (answers.myGuessNumber === UserGuess) {
+      console.log(chalk.yellow("Hurray! You guessed it right, Game Ends"));
+      numTries = 0;
+    } else {
+      console.log(chalk.red("You guessed it wrong"));
+      if (UserGuess > answers.myGuessNumber) {
+        console.log("Think Higher ");
+      } else {
+        console.log("Think Lower ");
+      }
+      console.log(`you have ${numTries - 1} left`);
+    }
+
+    numTries--;
+  }
+  const playAgainAnswers: playAgainType = await inquirer.prompt([
     {
-      type: "number",
-      name: "myGuessNumber",
-      message: "Enter a number between 1 to 10",
+      type: "confirm",
+      name: "playagain",
+      message: "Do you want to play again",
     },
   ]);
+  console.log("Play Again", playAgainAnswers);
+  if (playAgainAnswers.playagain === true) {
+    randomNumber = Math.floor(Math.random() * 10 + 1);
+    UserGuess = randomNumber;
 
-  console.log(answers);
-
-  if (answers.myGuessNumber === randomNumber) {
-    console.log(chalk.yellow("You guessed it right"));
+    numTries = 3;
   } else {
-    console.log(chalk.red("You guessed it wrong"));
+    play = false;
+    console.log("Existing Game");
   }
-  console.log(`you have ${numTries} left`);
-
-  numTries--;
 }
